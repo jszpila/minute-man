66f77dd0467039a0390734f271bab7151d7a4a3b
Jan Szpila <jszpila@nerdery.com>
2020-04-02 16:19:13 -0500

    Implements toggleable diagnostic info

--- src/components/InfoModal/index.tsx
+++ src/components/InfoModal/index.tsx
@@ -1,12 +1,21 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState, SyntheticEvent } from 'react';
 import Modal from '../Modal';
 import { AppContext } from '../../context/AppContext';
 import { isAppInstalled, isiOs } from '../AppBar/a2hsHelpers';
 import GitInfo from '../../static/GitInfo';
 
+import './infomodal.css';
+
 export default function InfoModal() {
   const env = process.env;
   const context = useContext(AppContext);
+  const [shouldShowDiagnotics, updateShouldShowDiagnotics] = useState(false);
+
+  function onToggleDiagnostics(event: SyntheticEvent): void {
+    event.preventDefault();
+    event.stopPropagation();
+    updateShouldShowDiagnotics(!shouldShowDiagnotics);
+  }
 
   return (
     <Modal
@@ -20,11 +29,12 @@ export default function InfoModal() {
           
           <p>{`${env.REACT_APP_DESCRIPTION}`}</p>
 
+          {/* TODO: break into own component? */}
           { context.shouldShowInstallButton &&
             <p>Click the install button to add it to the home screen of your phone, like an app!</p>
           }
 
-          {/* iOS does not support A2HS functioanlity; provide manual instructions */}
+          {/* iOS does not support A2HS functionality; provide manual instructions */}
           { isiOs() && !isAppInstalled() &&
             <p>
               <b>Add MinuteMan to your homescreen so you can use it like an app!</b>
@@ -36,9 +46,36 @@ export default function InfoModal() {
             </p>
           }
 
-          <p className="txt--muted">
-            {`${env.REACT_APP_NAME} v${env.REACT_APP_VERSION} by ${env.REACT_APP_AUTHOR}`}<br/>
-            {`${GitInfo.sha} / ${GitInfo.date}`}</p>
+          {/* TODO: break into component */}
+          <div className="diag-info txt--muted">
+            <button type="button"
+              className="toggle-diag-info-button"
+              onClick={onToggleDiagnostics}>
+              Diangostics
+              <i className="material-icons">{`${shouldShowDiagnotics ? "expand_less" : "expand_more"}`}</i>
+            </button>
+            <div className={`diag-info__body ${shouldShowDiagnotics ? "diag-info__body--active" : ""}`}>
+              <dl className="info">
+                <dt className="info-label">App</dt>
+                <dd className="info-value">
+                  {`${env.REACT_APP_NAME} v${env.REACT_APP_VERSION} by ${env.REACT_APP_AUTHOR}`}</dd>
+                <dt className="info-label">Build</dt>
+                <dd className="info-value">{`${GitInfo.sha} / ${GitInfo.date}`}</dd>
+                <dt className="info-label">Client</dt>
+                <dd className="info-value">
+                  <ul className="info-list">
+                    <li>
+                      <span className="info-label">Platform</span> 
+                      {`${navigator.platform}`}</li>
+                    <li><span className="info-label">User Agent</span>
+                      {`${navigator.userAgent}`}</li>
+                    <li><span className="info-label">Connected</span>
+                      {`${navigator.onLine}`}</li>
+                  </ul>
+                </dd>
+              </dl>
+            </div>
+          </div>
         </>
     </Modal>
   );
