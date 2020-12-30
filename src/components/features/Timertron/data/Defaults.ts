5d51586244484995408c9d189a99741ca2650cdd
Jan Szpila <jszpila@gmail.com>
2020-12-30 11:25:38 -0600

    Remove unused feature

--- src/components/features/Timertron/data/Defaults.ts
+++
@@ -1,9 +0,0 @@
- export const TimertronDefaults = {
-  isTimerActive: false,
-  intervalId: undefined,
-  timeElapsed: 0,
-  timeoutId: undefined,
-  isMicAccessGranted: false,
-  mediaStream: undefined,
-  audioProcessor: undefined,
-}
