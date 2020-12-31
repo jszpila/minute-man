42bdc8e396b5b17726cbea02427214500a497000
Jan Szpila <jszpila@gmail.com>
2020-12-31 16:46:01 -0600

    Updates field handling

--- src/components/features/Settings/components/ClickulatorSettings.tsx
+++ src/components/features/Settings/components/ClickulatorSettings.tsx
@@ -1,39 +1,25 @@
 /**
  *
- * Settings
- * App level settings
+ * Clickulator Settings
+ * Feature-level settings
  *
  */
 
 import { RouteComponentProps } from "@reach/router";
 import React, { useContext } from "react";
+import { FormattedMessage } from "react-intl";
 
-import randomizeInputName from "../../../../util/RandomizeInputName";
+import { getLocalizedDistanceUnit } from "../../../../util/L10n";
 import Field from "../../../Field";
+import FieldLabelWithHint from "../../../Field/FieldLabelWithHint";
 import AdjustmentSelect from "../../Clickulator/components/AdjustmentSelect";
 import ZeroAtDistanceInput from "../../Clickulator/components/ZeroAtDistanceInput";
 import { ClickulatorContext } from "../../Clickulator/context";
 import SettingsStore from "../SettingsStore";
-import { FormattedMessage } from "react-intl";
-import { getLocalizedDistanceUnit } from "../../../../util/L10n";
 
 export default function ClickulatorSettings(props: RouteComponentProps) {
   const context = useContext(ClickulatorContext);
   const settings = SettingsStore.getInstance();
-
-  const zeroLabel = (
-    <>
-      <FormattedMessage id="clickulator.zeroAtDistanceLabel" />
-      <i className="field__label__hint txt--smaller txt--muted">
-        ({getLocalizedDistanceUnit()})
-      </i>
-    </>
-  );
-
-  const inputNames = {
-    zeroDist: randomizeInputName("zeroAtDistance"),
-    opticInc: randomizeInputName("adjustmentIncrement"),
-  };
 
   function onZeroAtDistanceChange(value: number): void {
     settings.clickulator.zeroAtDistance = value;
@@ -50,21 +36,27 @@ export default function ClickulatorSettings(props: RouteComponentProps) {
       <legend className="form__fieldset__legend">
         <FormattedMessage id="settings.clickulator.title" />
       </legend>
-      <Field inputName={inputNames.zeroDist} labelText={zeroLabel}>
+      <Field
+        inputName="zeroAtDistance"
+        labelText={
+          <FieldLabelWithHint
+            messageId="clickulator.zeroAtDistanceLabel"
+            hintText={getLocalizedDistanceUnit()}
+          />
+        }
+      >
         <ZeroAtDistanceInput
-          name={inputNames.zeroDist}
           updaterFn={onZeroAtDistanceChange}
           value={settings.clickulator.zeroAtDistance}
         />
       </Field>
       <Field
-        inputName={inputNames.opticInc}
+        inputName="adjustmentIncrement"
         labelText={
           <FormattedMessage id="clickulator.adjustmentIncrementLabel" />
         }
       >
         <AdjustmentSelect
-          name={inputNames.opticInc}
           updaterFn={onAdjustmentIncrementChange}
           value={settings.clickulator.adjustmentIncrement}
         />
